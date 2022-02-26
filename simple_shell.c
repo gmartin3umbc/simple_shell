e9df21e6da8219b82246b6aa610afd05bf78fdf8
Gabriel <gabriel@CMSC241Debian>
2022-02-26 12:33:15 -0500

    basic shell part 1 works WITH MEMORY LEAKS

--- simple_shell.c
+++ simple_shell.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 #include "utils.h"
 
+
 char* readInput(){
-    
     //create initial buffer
     size_t bufferLength = 0;
     char* buffer = malloc(sizeof(char) * bufferLength);
 
-    getline(&buffer, &bufferLength, stdin);
+    ssize_t lastChar = getline(&buffer, &bufferLength, stdin);
+
+    //delete last char which is always the '\n' 
+    buffer[lastChar-1] = '\0';
     
     return buffer;
-
 }
 
 char** tokenize(char* buffer){
 
-    char** tokens = malloc(64*sizeof(char));
+    char** tokens = malloc(sizeof(buffer)*sizeof(char));
     int tokensIndex = 0;
     char* token;
 
@@ -25,29 +31,58 @@ char** tokenize(char* buffer){
         tokens[tokensIndex] = token;
         tokensIndex++;
     }
+    
+    free(token);
 
     return tokens;
+
+    free(tokens);
+}
+
+int executeInstruction(char** tokens){
+    
+    pid_t retVal;
+    int status;
+
+    retVal = fork();
+    if (retVal < 0) {
+        // Error forking
+        printf("fork() failed\n");
+        return 1;
+    } else if (retVal == 0) {
+        //execute instruction
+        execvp(tokens[0], tokens);
+    } else {
+        // Parent process
+        // while (!WIFEXITED(status) && !WIFSIGNALED(status)) {
+        // waitpid(retVal, &status, WUNTRACED);
+        wait(NULL);
+         
+    }
+
+    return 0;
 }
 
 int main()
 {
-    
+    char** tokens;
+    char* buffer;
     //test
-    printf("$");
-    char* buffer = readInput();
-
+    do
+    {
+        
+    printf("$ ");
+    buffer = readInput();
     buffer = unescape(buffer, NULL);
+    tokens = tokenize(buffer);
+    executeInstruction(tokens);    
 
-    char** tokens = tokenize(buffer);
+    } while (strcmp(tokens[0],"exit") != 0);
 
-    int i = 0;
-    while(tokens[i] != NULL){
-        printf("%s\n", tokens[i]);
-        i++;
-    }
+    free(tokens);
+    free(buffer);
 
     return 0;
-
 }
 
 
