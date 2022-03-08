e4fa4b7e676baaa08dc626d20322c6961bc38023
Gabriel <gabriel@CMSC241Debian>
2022-03-08 14:17:51 -0500

    DONE but need corrected exit and $

--- simple_shell.c
+++ simple_shell.c
@@ -7,145 +7,168 @@
 
 #include "utils.h"
 
+char *readInput()
+{
 
-char* readInput(){
-
-    
-    //create initial buffer
+    // create initial buffer
     size_t bufferLength = 0;
-    char* buffer = NULL;
+    char *buffer = NULL;
 
     ssize_t lastChar = getline(&buffer, &bufferLength, stdin);
 
-    //delete last char which is always the '\n'
-    buffer[lastChar-1] = '\0';
+    // delete last char which is always the '\n'
+    buffer[lastChar - 1] = '\0';
 
     return buffer;
-
 }
 
-char** tokenize(char* buffer){
+char **tokenize(char *buffer)
+{
 
-    //allocate memory for one pointer
-    char** tokens = malloc(sizeof(char*));
+    // allocate memory for one pointer
+    char **tokens = malloc(sizeof(char *));
     int tokensIndex = 0;
-    char* token;
+    char *token;
 
-    while((token = strtok_r(buffer, " ", &buffer))){
-        //allocate more memory for another pointer
-        tokens = realloc(tokens, (tokensIndex+1)*sizeof(char*));
+    while ((token = strtok_r(buffer, " ", &buffer)))
+    {
+        // allocate more memory for another pointer
+        tokens = realloc(tokens, (tokensIndex + 1) * sizeof(char *));
         tokens[tokensIndex] = token;
         tokensIndex++;
     }
 
-    //include final NULL pointer to be used in execvp
-    tokens = realloc(tokens, (tokensIndex+1)*sizeof(char*));
+    // include final NULL pointer to be used in execvp
+    tokens = realloc(tokens, (tokensIndex + 1) * sizeof(char *));
     tokens[tokensIndex] = NULL;
-    
+
     free(token);
     return tokens;
 }
 
-int executeInstruction(char** tokens){
-    
+int executeInstruction(char **tokens)
+{
     pid_t retVal;
     int status = 0;
 
     retVal = fork();
-    if (retVal < 0) {
+    if (retVal < 0)
+    {
         // Error forking
         printf("fork() failed\n");
         return 1;
-    } else if (retVal == 0) {
-        //execute instruction
-        execvp(tokens[0], tokens);
-    } else {
-        // Parent process
-        while (!WIFSIGNALED(status) && !WIFEXITED(status)) {
-        waitpid(retVal, &status, WUNTRACED);
-        }
-         
     }
-
-    return 0;
-}
-
-int main()
-{
-    char** tokens;
-    char* buffer;
-    char* unescaped;
-    char* instruction;
-
-    printf("$ ");
-    buffer = readInput();
-    unescaped = unescape(buffer, NULL);
-    tokens = tokenize(unescaped);
-    instruction = tokens[0];
-
-    if(strcmp(instruction, "proc") != 0){
-            executeInstruction(tokens); 
-    }
-
-    else{
-        char prefix[] = "/proc/";
-        char* fileName = strcat(prefix,tokens[1]);
-        
-        FILE * file = fopen(fileName, "r");
-        
-        char contents;
-        while ((contents = fgetc(file)) != EOF)
-        {
-            printf("%c", contents);
-        }
-        
-        printf("\n");
-        fclose(file);
-
-    }   
-
-    while (strcmp(instruction, "exit") != 0)
+    else if (retVal == 0)
     {
-        free(buffer);
-        free(unescaped);
-        free(tokens);
-
-        printf("$ ");
-
-        buffer = readInput();
-        unescaped = unescape(buffer, NULL);
-        tokens = tokenize(unescaped);
-        instruction = tokens[0];
-
-        if(strcmp(instruction, "proc") != 0){
-            executeInstruction(tokens); 
-        }
-
-        else{
-
-            char prefix[] = "/proc/";
-            char* fileName = strcat(prefix,tokens[1]);
-        
-            FILE * file = fopen(fileName, "r");
-        
-            char contents;
-            while ((contents = fgetc(file)) != EOF)
-            {
-                printf("%c", contents);
-            }
-        
-            printf("\n");
-            fclose(file);
-        } 
-
+        // execute instruction
+        execvp(tokens[0], tokens);
     }
-    
-
-    free(buffer);
-    free(unescaped);
-    free(tokens);    
+    else
+    {
+        // Parent process
+        while (!WIFSIGNALED(status) && !WIFEXITED(status))
+        {
+            waitpid(retVal, &status, WUNTRACED);
+        }
+    }
 
     return 0;
 }
 
+void executeProc(char *suffix)
+{
 
+    char prefix[] = "/proc/";
+    char *fileName = strcat(prefix, suffix);
+
+    FILE *file = fopen(fileName, "r");
+
+    char contents;
+
+    while ((contents = fgetc(file)) != EOF)
+    {
+        printf("%c", contents);
+    }
+
+    printf("\n");
+    fclose(file);
+}
+
+
+int main(int argc, char **argv)
+{
+
+    if (argc > 1)
+    {
+        fprintf(stderr, "Error: Programm does not accept any command line arguments\n");
+        exit(1);
+    }
+
+    else
+    {
+        {
+            char **tokens;
+            char *buffer;
+            char *unescaped;
+            char *instruction;
+
+            printf("$ ");
+            buffer = readInput();
+            unescaped = unescape(buffer, NULL);
+            tokens = tokenize(unescaped);
+            instruction = tokens[0];
+
+            if (strcmp(instruction, "proc") != 0)
+            {
+                executeInstruction(tokens);
+            }
+
+            else
+            {
+                executeProc(tokens[1]);
+            }
+
+            while (strcmp(instruction, "exit") != 0)
+            {
+                free(buffer);
+                free(unescaped);
+                free(tokens);
+
+                printf("$ ");
+
+                buffer = readInput();
+                unescaped = unescape(buffer, NULL);
+                tokens = tokenize(unescaped);
+                instruction = tokens[0];
+
+                if (strcmp(instruction, "proc") != 0)
+                {
+                    executeInstruction(tokens);
+                }
+
+                else
+                {
+                    char prefix[] = "/proc/";
+                    char *fileName = strcat(prefix, tokens[1]);
+
+                    FILE *file = fopen(fileName, "r");
+
+                    char contents;
+                    while ((contents = fgetc(file)) != EOF)
+                    {
+                        printf("%c", contents);
+                    }
+
+                    printf("\n");
+                    fclose(file);
+                }
+            }
+
+            free(buffer);
+            free(unescaped);
+            free(tokens);
+
+            return 0;
+        }
+    }
+}
